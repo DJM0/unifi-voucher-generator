62d53e579b7c972cfff2d4886f423043eea6e5c9
David Maitland <david.maitland@tdm.info>
2012-04-30 14:54:17 +0100

    Added files

---
+++ gen.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# Files needed
+pwd=`pwd`
+. $pwd/unifi-api.sh
+
+# Unifi connection infomation
+username=admin
+password=PASSWORD
+baseurl=https://CONTROLLERURL:8443
+
+# Generation settings
+time=60
+ammount=15
+note="TEST"
+
+# Generate vouchers
+unifi_login
+voucherID=`unifi_create_voucher $time $ammount $note`
+unifi_get_vouchers $voucherID > vouchers.tmp
+unifi_logout
+
+vouchers=`awk -F"[,:]" '{for(i=1;i<=NF;i++){if($i~/code\042/){print $(i+1)} } }' vouchers.tmp | sed 's/\"//g'`
+
+# HTML Settings
+line1="COMPANY WiFi Voucher"
+line2="60 minutes of use"
+
+# Build HTML
+
+rm vouchers.html
+
+echo '<html><head><link rel="stylesheet" href="style.css" /></head><body>' >> vouchers.html
+
+for code in $vouchers
+do
+    line3=$code
+    html='<div class="voucher"><div class="line1">'$line1'</div><div class="line2">'$line2'</div><div class="line3">'$line3'</div></div>'
+    echo $html >> vouchers.html
+done
+
+echo "</body></html>" >> vouchers.html
+
+firefox vouchers.html
---
+++ unifi-api.sh
@@ -0,0 +1,105 @@
+#!/bin/sh
+
+cookie=/tmp/unifi_cookie
+
+curl_cmd="curl --sslv3 --silent --cookie ${cookie} --cookie-jar ${cookie} --insecure "
+
+unifi_requires() {
+    if [ -z "$username" -o -z "$password" -o -z "$baseurl" ] ; then
+        echo "Error! please define required env vars before including unifi_sh. E.g. "
+        echo ""
+        echo "username=ubnt"
+        echo "password=ubnt"
+        echo "baseurl=http://unifi:8443"
+        echo ""
+        exit -1
+    fi
+}
+
+unifi_login() {
+    # authenticate against unifi controller
+    ${curl_cmd} --data "login=login" --data "username=$username" --data "password=$password" $baseurl/login
+}
+
+unifi_logout() {
+    # logout
+    ${curl_cmd} $baseurl/logout
+}
+
+unifi_authorize_guest() {
+    if [ $# -lt 2 ] ; then
+        echo "Usage: $0 <mac> <minutes>"
+        exit -1
+    fi
+
+    mac=$1
+    minutes=$2
+
+	${curl_cmd} --data "json={'cmd':'authorize-guest', 'mac':'${mac}', 'minutes':${minutes}}" $baseurl/api/cmd/stamgr
+}
+
+unifi_reconnect_sta() {
+    if [ $# -lt 1 ] ; then
+        echo "Usage: $0 <mac>"
+        exit -1
+    fi
+
+    mac=$1
+
+    ${curl_cmd} --data "json={'cmd':'kick-sta', 'mac':'${mac}'}" $baseurl/api/cmd/stamgr
+}
+
+
+unifi_block_sta() {
+    if [ $# -lt 1 ] ; then
+        echo "Usage: $0 <mac>"
+        exit -1
+    fi
+
+    mac=$1
+
+    ${curl_cmd} --data "json={'cmd':'block-sta', 'mac':'${mac}'}" $baseurl/api/cmd/stamgr
+}
+
+unifi_list_sta() {
+    ${curl_cmd} --data "json={}" $baseurl/api/stat/sta
+}
+
+unifi_backup() {
+    if [ "$1" = "" ]; then
+        output=unifi-backup.unf # or `date +%Y%m%d`.unf
+    else
+        output=$1
+    fi
+
+    # ask controller to do a backup, response contains the path to the backup file
+    path=`$curl_cmd --data "json={'cmd':'backup'}" $baseurl/api/cmd/system | sed -n 's/.*\(\/dl.*unf\).*/\1/p'`
+
+    # download the backup to the destinated output file
+    $curl_cmd $baseurl$path -o $output
+}
+
+unifi_create_voucher() {
+    if [ $# -lt 2 ] ; then
+        echo "Usage: $0 <minutes> <n> [note]"
+        exit -1
+    fi
+    minutes=$1
+    n=$2
+    [ "$3" != "" ] && note=", 'note':'$3' "
+    token=`${curl_cmd} --data "json={'cmd':'create-voucher','expire':${minutes},'n':$n $note}" $baseurl/api/cmd/hotspot \
+    	| sed -e 's/.*"create_time"\s*:\s*\([0-9]\+\).*/\1/'`
+    echo $token
+}
+
+unifi_get_vouchers() {
+    if [ $# -lt 1 ] ; then
+        echo "Usage: $0 <token>"
+        exit -1
+    fi
+    token=$1
+    ${curl_cmd} --data "json={'create_time':${token}}" $baseurl/api/stat/voucher
+}
+
+unifi_requires
+
